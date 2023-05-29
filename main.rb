d86997289f8a56d6770069740bb66250f9b590a8
lianaa98 <liana.kan98@gmail.com>
2023-05-29 13:43:22 -0700

    finish game

--- main.rb
+++ main.rb
@@ -2,9 +2,6 @@ require './player.rb'
 require './random_calculator.rb'
 require './evaluator.rb'
 
-# Evaluator
-# Player response to answer
-
 class Game
 
   def initialize
@@ -24,6 +21,8 @@ class Game
 
   def start
 
+    loop do
+
     if @current_player == @eval.player1
       print "Player 2: "
       round
@@ -37,19 +36,32 @@ class Game
       print "Player 1: "
       round
       if @current_answer == true
-        puts "Player 2: YES! You are correct!"
+        puts "Player 1: YES! You are correct!"
         @eval.player2.score -= 1
       else
-        puts "Player 2: Seriously? No!"
+        puts "Player 1: Seriously? No!"
       end
     end
 
     current_score
+    round_ends
+    if (@eval.player1.score == 1 || @eval.player2.score == 1)
+      break
+    else
+      puts "----- NEW TURN -----"
+    end
 
-
-
-
-
+  end
+  
+    if (@eval.player1.score == 1)
+      puts "Player 1 wins with a score of 1/3"
+      puts "----- GAME OVER -----"
+      puts "Good bye!"
+    else
+      puts "Player 2 wins with a score of 1/3"
+      puts "----- GAME OVER -----"
+      puts "Good bye!"
+    end
   end
 
   def round
@@ -76,8 +88,3 @@ end
 
 game = Game.new
 game.start
-
-# while no one gets to 1/3, continue looping game 
-
-
-# New Turn / Game Over line
